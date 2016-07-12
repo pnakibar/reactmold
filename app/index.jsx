41933a7dbca9817072636c7bd43776035289dd97
Pedro Mathias Nakibar <pedronakibar@gmail.com>
2016-07-12 00:19:36 -0300

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-const app = () => <p>Hello World!</p>
+const App = () => <p>Hello World!</p>
 
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
